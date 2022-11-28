f7d16ed1fb85dee5f1a39ebd382c57c97f05f54c
luffy-arc <115520477+luffy-arc@users.noreply.github.com>
2022-11-28 11:15:27 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,13 @@
+dict = {'Ari': '081267888', 'dina': '087677776',}
+dict['riko'] = "087654544"
+dict['dina'] = "088999776"
+
+del dict['dina'];
+
+
+print ("Nama  |  Nomor Telepon")
+print ("========================")
+print ("Ari",' ', "|", dict['Ari'])
+print("riko",'', "|", dict['riko'])
+
+#latihan 1
